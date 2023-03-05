3c0f19a041239a285319e8cefad25105f96db456
kouhei525 <kouhei.dogfather525@gmail.com>
2023-03-05 12:42:23 +0900

    req

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from PIL import Image
+from dogncat.model import predict
+
+st.set_option("deprecation.showfileUploaderEncoding", False)
+st.sidebar.title("Take a photo of your pets")
+st.sidebar.write("Let me make a guess about breeds of your pets")
+st.sidebar.write("")
+img_source = st.sidebar.radio("How upload?",("select file","take a photo"))
+if img_source == "select file":
+    img_file = st.sidebar.file_uploader("Choose the photo",type=["png","jpg","jpeg"])
+elif img_source == "take a photo":
+    img_file = st.camera_input("Let's shoot")
+
+if img_file is not None:
+    with st.spinner("guessing..."):
+        img = Image.open(img_file)
+        st.image(img, caption="Target", width=480)
+        st.write("")
+        result = predict(img)
+        st.write(result)
+
+st.sidebar.caption("""Dataset : The Oxford-IIIT Pet Dataset\n
+Licence is Creative Commons Attribution-ShareAlike 4.0 International License.""")
---
+++ model.py
@@ -0,0 +1,65 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+from PIL import Image
+
+IMG_MEAN = [0.485, 0.456, 0.406]
+IMG_STD = [0.229, 0.224, 0.225]
+class_name = ['abyssinian',
+ 'american_bulldog',
+ 'american_pit_bull_terrier',
+ 'basset_hound',
+ 'beagle',
+ 'bengal',
+ 'birman',
+ 'bombay',
+ 'boxer',
+ 'british_shorthair',
+ 'chihuahua',
+ 'egyptian_mau',
+ 'english_cocker_spaniel',
+ 'english_setter',
+ 'german_shorthaired',
+ 'great_pyrenees',
+ 'havanese',
+ 'japanese_chin',
+ 'keeshond',
+ 'leonberger',
+ 'maine_coon',
+ 'miniature_pinscher',
+ 'newfoundland',
+ 'persian',
+ 'pomeranian',
+ 'pug',
+ 'ragdoll',
+ 'russian_blue',
+ 'saint_bernard',
+ 'samoyed',
+ 'scottish_terrier',
+ 'shiba_inu',
+ 'siamese',
+ 'sphynx',
+ 'staffordshire_bull_terrier',
+ 'wheaten_terrier',
+ 'yorkshire_terrier']
+
+model = torchvision.models.resnet18(weights=True)
+model.fc = nn.Linear(512, 37)
+param = torch.load("dog_cat.pth", map_location=torch.device('cpu'))
+model.load_state_dict(param)
+
+def predict(path):
+    img = Image.open(path)
+    transform = transforms.Compose([transforms.Resize(256), transforms.CenterCrop(224),transforms.ToTensor(),transforms.Normalize(IMG_MEAN, IMG_STD)])
+    x = transform(img)
+    model.eval()
+    outputs = model(x)
+    y = torch.argmax(outputs)
+    return class_name[y]
+
+w = predict("/Users/kouhei/Documents/python_code/IMG_0543.JPG")
+print(w)
